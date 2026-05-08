1f8ebfbb610ab63f154e2a14548daa04bf31de10
KwstasKaska <round9876@gmail.com>
2026-05-08 13:33:55 +0300

    deleted seed

---
+++ src/utils/utensils.ts
@@ -0,0 +1,47 @@
+export enum CookingVessels {
+  Steamer = 'Ατμομάγειρας',
+  DutchOven = 'Γάστρα',
+  Wok = 'Γουόκ',
+  Pot = 'Κατσαρόλα',
+  Pan = 'Τηγάνι',
+  Oven = 'Φούρνος',
+  AirFryer = 'Φριτέζα Αέρα',
+  PressureCooker = 'Χύτρα Ταχύτητας',
+}
+
+export enum CookingVesselsEN {
+  Steamer = 'Steamer',
+  DutchOven = 'Dutch Oven',
+  Wok = 'Wok',
+  Pot = 'Pot',
+  Pan = 'Pan',
+  Oven = 'Oven',
+  AirFryer = 'Air Fryer',
+  PressureCooker = 'Pressure Cooker',
+}
+
+export enum Utensils {
+  Ladle = 'Κουτάλα',
+  Spoon = 'Κουτάλι',
+  Knife = 'Μαχαίρι',
+  Fork = 'Πιρούνι',
+}
+
+export enum UtensilsEN {
+  Ladle = 'Ladle',
+  Spoon = 'Spoon',
+  Knife = 'Knife',
+  Fork = 'Fork',
+}
+
+export enum ExtraEquipment {
+  Blender = 'Μπλέντερ',
+  CuttingBoard = 'Ξύλο Κοπής',
+  Grater = 'Τρίφτης',
+}
+
+export enum ExtraEquipmentEN {
+  Blender = 'Blender',
+  CuttingBoard = 'Cutting Board',
+  Grater = 'Grater',
+}
