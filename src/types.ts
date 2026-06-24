1179a7459fc41e5fd8432ae87b7e5ba166c46bee
KwstasKaska <round9876@gmail.com>
2026-06-24 12:14:25 +0300

    removed unused dependecies

--- src/types.ts
+++ src/types.ts
@@ -3,5 +3,5 @@ import Redis from 'ioredis';
 export type MyContext = {
   req: any; // express αντικείμενο για τις αιτήσεις
   res: any; // express αντικείμενο για την απαντηση του express
-  redis: Redis | null; //χρησιμοποιείται για διαδικασίες όπως το forgot password
+  redis: Redis | null;
 };
